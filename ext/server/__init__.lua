d3e802dd3c034409a048c253884b05152c82a3c8
thysw95 <thys.wentzel@africonnekt.com>
2023-10-14 12:24:15 +0200

    Implemented a test method to add players to the ranking list and increase their exp on the score event

--- ext/server/__init__.lua
+++ ext/server/__init__.lua
@@ -1,19 +1,91 @@
 require("__shared/config")
+-- require("SQLTest")
+
+playerRankClass = require('__shared/PlayerRank')
+currentPlayers = {}
+
+function PlayerXPUpdated(player, score)
+    if #currentPlayers > 0 then
+        for playerIndex = #currentPlayers, 1, -1 do
+            if currentPlayers[playerIndex]['r_PlayerName'] == player.name then
+                -- self.r_PlayerCurrentXP = self.r_PlayerCurrentXP + xpValue
+                -- currentPlayers[i]['r_PlayerCurrentXP'] = currentPlayers[i]['r_PlayerCurrentXP']
+                print("Found a player to increase XP!!!!")
+
+                IncreaseGeneralPlayerXP(playerIndex, score)
+            end
+        end
+    end
+end
+
+function IncreaseGeneralPlayerXP(playerIndex, xpValue)
+    currentPlayers[playerIndex]['r_PlayerCurrentXP'] = currentPlayers[playerIndex]['r_PlayerCurrentXP'] + xpValue
+
+    -- print("GAINED EXPERIENCE!!!!!")
+    -- print("CURRENT EXPERIENCE:")
+    -- print(currentPlayers[playerIndex]['r_PlayerCurrentXP'])
+
+    if currentPlayers[playerIndex]['r_PlayerCurrentXP'] >= currentPlayers[playerIndex]['r_PlayerRequiredXP'] then
+        GeneralLevelUp(playerIndex)
+    end
+end
+
+function GeneralLevelUp(playerIndex)
+    currentPlayers[playerIndex]['r_PlayerRequiredXP'] = currentPlayers[playerIndex]['r_PlayerRequiredXP'] + (currentPlayers[playerIndex]['r_PlayerRequiredXP'])
+    currentPlayers[playerIndex]['r_PlayerLevel'] = currentPlayers[playerIndex]['r_PlayerLevel'] + 1
+
+    -- print(currentPlayers[playerIndex]['r_PlayerName'] .. " HAS GAINED A LEVEL!!!!")
+    -- print("NEW LEVEL IS: ")
+    -- print(currentPlayers[playerIndex]['r_PlayerLevel'])
+    
+end
 
 Events:Subscribe('Player:Score', function(player, scoringTypeData, score)
-    local rankPlayer = PlayerManager:GetPlayerByName(playerName)
+    -- local rankPlayer = PlayerManager:GetPlayerByName(playerName)
 
-    if rankPlayer.name == player.name then
-        NetEvents:Broadcast('PlayerScoreEvent', score)
-    end
+    -- if rankPlayer.name == player.name then
+    --     NetEvents:Broadcast('PlayerScoreEvent', score)
+    -- end
+    PlayerXPUpdated(player, score)
 end)
 
 Events:Subscribe('Player:Joining', function(name, playerGuid, ipAddress, accountGuid)
-    print('PLAYER JOINING OWO!!!!!!!!!')
-    print('Player Name:')
-    print(name)
-    print('Player Guid:')
-    print(playerGuid)
-    print('Account Guid')
-    print(accountGuid)
+    -- print('PLAYER JOINING OWO!!!!!!!!!')
+    -- print('Player Name:')
+    -- print(name)
+    -- print('Player Guid:')
+    -- print(playerGuid)
+    -- print('Account Guid')
+    -- print(accountGuid)
+
+    -- local player = PlayerManager:GetPlayersByName(playerGuid)
+
+    -- -- testSQL()
+    -- if player ~= nil then
+    --     print("New player found!!!! Adding to the stats list now")
+
+    --     local playerRankObject = playerRankClass(player)
+
+    --     table.insert(currentPlayers, playerRankObject)
+    -- end
+
+    
+end)
+
+Events:Subscribe('Player:Created', function(player)
+    -- local playerRankObject = playerRankClass(player)
+
+    -- print("THE RECEIVED PLAYER OBJECT IS: ")
+    -- print(player)
+
+    -- table.insert(currentPlayers, playerRankObject)
+
+end)
+
+NetEvents:Subscribe('AddNewPlayerForStats', function(player, data)
+    print(player.name .. ' is joining.')
+    print(data)
+
+    local playerRankObject = playerRankClass(player)
+    table.insert(currentPlayers, playerRankObject)
 end)
---
+++ ext/shared/PlayerRank.lua
@@ -0,0 +1,48 @@
+local VUPlayerRankClass = class('VUPlayerRank')
+
+function VUPlayerRankClass:__init(player)
+    -- print("I RECEIVED THE FOLLOWING PLAYER FOR OBJECT CREATION:")
+    -- print(player)
+	self.InitVariables(self, player)
+end
+
+function VUPlayerRankClass:InitVariables(player)
+    self.InitPlayerVariables(self, player)
+    self.InitGeneralRankStats(self)
+
+    -- print("SUCCESSFULLY ADDED INITIALIZED A NEW PLAYER!!!!!!!!!")
+end
+
+function VUPlayerRankClass:InitPlayerVariables(player)
+    self.r_PlayerName = player.name
+end
+
+function VUPlayerRankClass:InitGeneralRankStats()
+    self.r_PlayerLevel = 1
+    self.r_PlayerCurrentXP = 0
+    self.r_PlayerRequiredXP = 200
+end
+
+-- function VUPlayerRankClass:OnExperienceGained(xpValue)
+--     self.r_PlayerCurrentXP = self.r_PlayerCurrentXP + xpValue
+
+--     print("GAINED EXPERIENCE!!!!!")
+--     print("CURRENT EXPERIENCE:")
+--     print(self.r_PlayerCurrentXP)
+
+--     if self.r_PlayerCurrentXP >= self.r_PlayerRequiredXP then
+--         self.LevelUp(self)
+--     end
+-- end
+
+-- function VUPlayerRankClass:LevelUp()
+--     self.r_PlayerRequiredXP = self.r_PlayerRequiredXP + (self.r_PlayerRequiredXP)
+--     self.r_PlayerLevel = self.r_PlayerLevel + 1
+
+--     print(self.r_PlayerName .. " HAS GAINED A LEVEL!!!!")
+--     print("NEW LEVEL IS: ")
+--     print(self.r_PlayerLevel)
+    
+-- end
+
+return VUPlayerRankClass
