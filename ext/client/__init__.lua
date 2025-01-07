342349bfc037900a126dc5f8e57d39e9b44668b1
Doc <42835383+Doc-ice@users.noreply.github.com>
2025-01-07 12:36:30 +0000

    updated

--- ext/client/__init__.lua
+++ ext/client/__init__.lua
@@ -1,8 +1,6 @@
 Hooks:Install('UI:PushScreen', 999, function(hook, screen, graphPriority, parentGraph)
 	local screen = UIGraphAsset(screen)
- if  screen.name == 'UI/Flow/Screen/SpawnScreenTicketCounterTDMScreen' or
-            screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsScreen' or
-            screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD32Screen' or
+ if  screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD32Screen' or
             screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD16Screen' or
             screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD64Screen' then
 	hook:Return()
